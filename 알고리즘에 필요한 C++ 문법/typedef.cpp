bd703ca3c388d8ca4c4f0b43ab5e59a8f6e60f7f
Sang jin Lee <w1004123@gmail.com>
2023-01-16 22:57:50 +0900

    first commit

---
+++ 알고리즘에 필요한 C++ 문법/typedef.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+typedef int i; //int를 i라는 새로운 별칭으로 바꿔서 표현
+i main(){
+    i a = 1;
+    cout << a << "\n";
+    return 0;
+}
---
+++ 알고리즘에 필요한 C++ 문법/입력과 출력.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+string a;
+
+int main() {
+	cin >> a; //scanf
+	cout << a << "\n"; //printf
+	return 0;
+}
