47baa91c674aec473eb151274b3bff8302a2406b
Sang jin Lee <w1004123@gmail.com>
2023-01-17 13:23:29 +0900

    23.1.17

---
+++ 알고리즘에 필요한 C++ 문법/입력/입력.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+string a;
+int main(){
+    cin >> a; //scanf
+    cout << a << "\n"; //printf
+    return 0;
+}
+
+// cin을 쓰는 이유 : cin은 개행문자( 띄어쓰기, 엔터)까지 입력받기 때문
