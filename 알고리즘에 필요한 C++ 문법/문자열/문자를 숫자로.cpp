fe638220e3d20701d0d806201d9b847822de516c
Sang jin Lee <w1004123@gmail.com>
2023-01-18 17:11:23 +0900

    23-1-18

---
+++ 알고리즘에 필요한 C++ 문법/문자열/문자를 숫자로.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char a = 'a';
+    cout << (int)a << '\n';
+    cout << (int)a - 97 << '\n';
+    cout << (int)a - 'a';
+    return 0;
+}
---
+++ 알고리즘에 필요한 C++ 문법/오버,언더 플로/언더플로.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = -2147483648;
+    cout << a << '\n';
+    a--;
+    cout << a << '\n';
+    return 0;
+}
